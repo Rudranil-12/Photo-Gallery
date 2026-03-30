e110612add12cfe7aa607254ed09141786a98257
Rudranil Mallick <rudranilmallick38@gmail.com>
2026-03-30 23:24:42 +0530

    all file upload

---
+++ src/hooks/useFetchPhotos.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetchPhotos = () => {
+
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const fetchPhotos = async () => {
+
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        "https://picsum.photos/v5/list?limit=30"
+      );
+
+      setPhotos(res.data);
+
+    } catch (err) {
+
+      setError("Failed to fetch photos");
+    } finally {
+
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+
+    fetchPhotos();
+
+  }, []);
+  return { photos, loading, error };
+
+};
+
+export default useFetchPhotos;
